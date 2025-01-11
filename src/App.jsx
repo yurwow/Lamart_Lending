ffaede39ce844cca7ea5c7acf639c174bc8cb100
Yuriy Kuzin <yura_kuzin_03@mail.ru>
2025-01-11 11:31:50 +0500

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import LandingPage from "./components/LandingPage/LandingPage.jsx";
 import AdminPanel from "./components/Admin/AdminPanel.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes} from "react-router-dom";
 import LoginPage from "./components/LoginPage/LoginPage.jsx";
 import RegisterUser from "./components/Admin/Register.jsx";
 import AdminHome from "./components/AdminPanel/AdminHome/AdminHome.jsx";
@@ -10,7 +10,7 @@ import SecondAdmin from "./components/SecondAdmin/SecondAdmin.jsx";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="*" element={<NotFound/>}/>
@@ -21,7 +21,7 @@ function App() {
                 <Route path="/adminpanel" element={<AdminHome/>}/>
                 <Route path="/second" element={<SecondAdmin/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
