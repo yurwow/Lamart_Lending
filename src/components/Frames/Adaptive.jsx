bd8b53bcd832daabc83e0d3818ade17b56ecebed
Yuriy Kuzin <yura_kuzin_03@mail.ru>
2025-01-15 00:00:46 +0500

    Адаптив

---
+++ src/components/Frames/Adaptive.jsx
@@ -0,0 +1,27 @@
+import HeaderFrame from "./HeaderFrame/HeaderFrame.jsx";
+import ComputerFrame from "./ComputerFrame/ComputerFrame.jsx";
+import PrivilegeFrame from "./PrivilegeFrame/PrivilegeFrame.jsx";
+import WhyUsFrame from "./WhyUsFrame/WhyUsFrame.jsx";
+import FormFrame from "./FormFrame/FormFrame.jsx";
+import ClientsFrame from "./ClientsFrame/ClientsFrame.jsx";
+import ReviewsFrame from "./ReviewsFrame/ReviewsFrame.jsx";
+import FAQFrame from "./FAQFrame/FAQFrame.jsx";
+import FooterFrame from "./FooterFrame/FooterFrame.jsx";
+
+const Adaptive = () => {
+    return (
+        <>
+            <HeaderFrame/>
+            <ComputerFrame/>
+            <PrivilegeFrame/>
+            <WhyUsFrame/>
+            <FormFrame/>
+            <ClientsFrame/>
+            <ReviewsFrame/>
+            <FAQFrame/>
+            <FooterFrame/>
+        </>
+    );
+};
+
+export default Adaptive;
